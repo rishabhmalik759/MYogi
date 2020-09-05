7413793de4ea588001ecbff46ada760dfabb3561
rishabhmalik759 <rishabhmalik759@hotmail.com>
2020-09-05 06:58:30 +0530

    reducerModal

--- frontend-ionic/src/store/reducers/index.ts
+++ frontend-ionic/src/store/reducers/index.ts
@@ -1,3 +1,4 @@
 import { combineReducers } from 'redux';
+import { modalReducer } from './modalReducer';
 
-export default combineReducers({});
+export default combineReducers({ modalReducer });
--- frontend-ionic/src/store/reducers/modalReducer.ts
+++ frontend-ionic/src/store/reducers/modalReducer.ts
@@ -3,9 +3,14 @@ import { IModalAction } from '../actions';
 import * as types from '../types';
 import { InitialAppState } from '../initialState';
 
-const modalReducer = (state = InitialAppState, action: IModalAction) => {
+export const modalReducer = (state = InitialAppState, action: IModalAction) => {
+  const { payload } = action;
   switch (action.type) {
     case types.SHOW_MODAL:
-      return { ...state, modal: { modalActive: true } };
+      return { ...state, modal: payload };
+    case types.HIDE_MODAL:
+      return { ...state, modal: payload };
+    default:
+      return state;
   }
 };
