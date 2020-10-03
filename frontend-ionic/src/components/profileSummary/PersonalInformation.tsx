86e254aadc1c77ba61be6545056ff3400f1f53e4
rishabhmalik759 <rishabhmalik759@hotmail.com>
2020-10-03 15:49:50 +0200

    added styles to profile

--- frontend-ionic/src/components/profileSummary/PersonalInformation.tsx
+++ frontend-ionic/src/components/profileSummary/PersonalInformation.tsx
@@ -1,136 +1,147 @@
 import {
-	IonAvatar,
-	IonButton,
-	IonButtons,
-	IonCard,
-	IonCardContent,
-	IonCardHeader,
-	IonCardSubtitle,
-	IonCardTitle,
-	IonChip,
-	IonContent,
-	IonFab,
-	IonFabButton,
-	IonFabList,
-	IonHeader,
-	IonIcon,
-	IonInput,
-	IonItem,
-	IonLabel,
-	IonList,
-	IonListHeader,
-	IonPopover,
-	IonTitle,
-	IonToolbar,
+  IonBadge,
+  IonButtons,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonChip,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonPopover,
+  IonText,
 } from '@ionic/react';
-import {
-	logoFacebook,
-	logoInstagram,
-	logoLinkedin,
-	logoTwitter,
-	logoYoutube,
-	pin,
-	text,
-	ellipsisVerticalCircle,
-	pencil,
-	arrowForwardCircle,
-} from 'ionicons/icons';
+import { pencil } from 'ionicons/icons';
 
 import React, { Fragment, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { AppState } from '../../store/reducers';
-import ExploreContainer from '../shared/ExploreContainer';
 
 const PersonalInformation: React.FC = () => {
-	const {
-		name,
-		avatar,
-		email,
-		weight,
-		height,
-		type,
-		sex,
-		age,
-		bmi,
-		level,
-		country,
-	} = useSelector((state: AppState) => state.userState);
-	const [showPopover, setShowPopover] = useState<{
-		open: boolean;
-		event: Event | undefined;
-	}>({
-		open: false,
-		event: undefined,
-	});
-	return (
-		<Fragment>
-			<div className='col-md-8 center'>
-				<IonCard className='p-0 background-light border-light'>
-					<div>
-						<div className='parent'> </div>
-						<div
-							className='card ml-4 '
-							style={{
-								zIndex: 2,
-								width: '8em',
-								top: -20,
-								position: 'relative',
-								borderRadius: '50%',
-							}}
-						>
-							{' '}
-							<img style={{ borderRadius: '50%' }} src={avatar}></img>
-						</div>
-					</div>
-					<div className='row'>
-						<div className='col-md-10'></div>
-						<div className='col text-dark'>
-							<IonButtons slot='end'>
-								<IonPopover
-									isOpen={showPopover.open}
-									event={showPopover.event}
-									onDidDismiss={(e) =>
-										setShowPopover({ open: false, event: undefined })
-									}
-								>
-									<IonList>
-										<IonListHeader>
-											<strong>Profile Options</strong>
-										</IonListHeader>
-										<IonItem button>Complete Profile</IonItem>
-										<IonItem button>Add Disorder</IonItem>
-										<IonItem button>Edit Profile</IonItem>
-										<IonItem
-											lines='none'
-											detail={false}
-											button
-											onClick={(e) =>
-												setShowPopover({ open: false, event: undefined })
-											}
-										>
-											Close
-										</IonItem>
-									</IonList>{' '}
-								</IonPopover>
+  const {
+    name,
+    avatar,
+    email,
+    weight,
+    height,
+    type,
+    sex,
+    age,
+    bmi,
+    level,
+    country,
+  } = useSelector((state: AppState) => state.userState);
+  const [showPopover, setShowPopover] = useState<{
+    open: boolean;
+    event: Event | undefined;
+  }>({
+    open: false,
+    event: undefined,
+  });
+  return (
+    <Fragment>
+      <div className="col-md-8 center">
+        <IonCard className="p-0 background-light border-light">
+          <div className="profile-background"> </div>
+          <div className="card ml-4 profile-photo">
+            <img src={avatar}></img>
+          </div>
 
-								<IonFab vertical='bottom' horizontal='end' slot='fixed'>
-									<IonFabButton
-										size='small'
-										onClick={(e) =>
-											setShowPopover({ open: true, event: e.nativeEvent })
-										}
-									>
-										<IonIcon icon={pencil} />
-									</IonFabButton>
-								</IonFab>
-							</IonButtons>
-						</div>
-					</div>
-					<p>Something is crazy</p>
-				</IonCard>
-			</div>
-		</Fragment>
-	);
+          <div className="edit-profile-button">
+            <IonButtons slot="end">
+              <IonFab vertical="bottom" horizontal="end" slot="fixed">
+                <IonFabButton
+                  size="small"
+                  onClick={(e) =>
+                    setShowPopover({ open: true, event: e.nativeEvent })
+                  }
+                >
+                  <IonIcon icon={pencil} />
+                </IonFabButton>
+              </IonFab>{' '}
+            </IonButtons>
+          </div>
+          <div className="left-align">
+            <IonText>
+              <h3>{name}</h3>
+            </IonText>
+            <IonText>
+              <p>{level}</p>
+            </IonText>
+          </div>
+          <div>
+            <IonList className="profile-list">
+              <IonItem>
+                <IonLabel color="primary">BMI</IonLabel>
+                <IonBadge color="primary">23</IonBadge>
+              </IonItem>
+            </IonList>
+          </div>
+          <div className="left-align">
+            <IonChip color="primary">
+              <IonLabel>
+                <strong>BMI</strong> &nbsp;&nbsp;&nbsp;&nbsp;23
+              </IonLabel>
+            </IonChip>
+          </div>
+          <div className="row  profile-personal-card center">
+            <IonCard slot="start">
+              <IonCardHeader>
+                <IonCardTitle>Age</IonCardTitle>
+              </IonCardHeader>
+              <IonCardContent>25</IonCardContent>
+            </IonCard>
+            <IonCard>
+              <IonCardHeader>
+                <IonCardTitle>Weight</IonCardTitle>
+              </IonCardHeader>
+              <IonCardContent>25</IonCardContent>
+            </IonCard>
+            <IonCard>
+              <IonCardHeader>
+                <IonCardTitle>Height</IonCardTitle>
+              </IonCardHeader>
+              <IonCardContent>25</IonCardContent>
+            </IonCard>
+          </div>
+          <div className="col text-dark">
+            <IonPopover
+              isOpen={showPopover.open}
+              event={showPopover.event}
+              onDidDismiss={(e) =>
+                setShowPopover({ open: false, event: undefined })
+              }
+            >
+              <IonList>
+                <IonListHeader>
+                  <strong>Profile Options</strong>
+                </IonListHeader>
+                <IonItem button>Complete Profile</IonItem>
+                <IonItem button>Add Disorder</IonItem>
+                <IonItem button>Edit Profile</IonItem>
+                <IonItem
+                  lines="none"
+                  detail={false}
+                  button
+                  onClick={(e) =>
+                    setShowPopover({ open: false, event: undefined })
+                  }
+                >
+                  Close
+                </IonItem>
+              </IonList>{' '}
+            </IonPopover>
+          </div>
+        </IonCard>
+      </div>
+    </Fragment>
+  );
 };
 
 export default PersonalInformation;
